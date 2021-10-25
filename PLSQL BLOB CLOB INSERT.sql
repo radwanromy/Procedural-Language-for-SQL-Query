a3da58ab3b282d73f6c189afba22606815c197eb
radwanromy <radwanromy@gmail.com>
2021-10-26 03:05:29 +0600

    PLSQL BLOB CLOB INSERT

---
+++ PLSQL BLOB CLOB INSERT.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (employee_id, first_name,last_name, email, hire_date, job_id, 						salary, resume, picture)
+VALUES (405, 'Marvin', 'Ellis', 'MELLIS', SYSDATE, 		 'AD_ASST', 4000, EMPTY_CLOB(),NULL);
